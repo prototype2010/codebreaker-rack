beb33e20ca5273adb635cd3de399b8d8842911b5
prototype2010 <Firstday2010>
2021-04-08 18:06:47 +0300

    minor stuff

--- middlewares/redirect_to_active_game.rb
+++ middlewares/redirect_to_active_game.rb
@@ -2,7 +2,7 @@ class RedirectToActiveGame < BaseMiddleware
   def call(env)
     request_params(env)
 
-    return @app.call(env) if @game.nil?
+    return @app.call(env) unless @game
     return @app.call(env) if @game.lose? || @game.win?
     return @app.call(env) if @path == Constants::GAME_PATH
 
