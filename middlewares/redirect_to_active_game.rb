dbc6292792399c0aac37f03971e8495326ea2bac
prototype2010 <Firstday2010>
2021-04-08 18:48:30 +0300

    rubocop

--- middlewares/redirect_to_active_game.rb
+++ middlewares/redirect_to_active_game.rb
@@ -1,9 +1,9 @@
 class RedirectToActiveGame < BaseMiddleware
   def proceed
     return call_next if !@game ||
-      @game.lose? ||
-      @game.win? ||
-      @path == Constants::GAME_PATH
+                        @game.lose? ||
+                        @game.win? ||
+                        @path == Constants::GAME_PATH
 
     redirect(Constants::GAME_PATH)
   end
